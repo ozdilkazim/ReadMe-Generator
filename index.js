f1352abf829c841821bb69ad3b5e9f78dd23823b
Kazim Ozdil <skozdil@gmail.com>
2024-02-16 20:16:44 +0000

    basic code added

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const fs = require("fs");
-const path = require('path');
+const path = require('path'); // Optional
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name : 'title',
+        message : 'What is the title of Read Me file?'
+    }
 ];
-
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
 function init() {
-
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        fs.writeFile('log.txt', JSON.stringify(answers), (err)=> {
+            console.log(err);
+        })
+    });
 }
 
 // function call to initialize program
